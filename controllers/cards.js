8c74c31716e61a2d0f4686934ba311dd1a4e464d
Vladimir Iakhnev <Yahnev91@yandex.ru>
2022-08-30 20:55:51 +0300

    ПР13 ошибки в тестах

--- controllers/cards.js
+++ controllers/cards.js
@@ -58,12 +58,12 @@ module.exports.likeCard = async (req, res) => {
     const cards = await Card.findByIdAndUpdate(CardId, { $addToSet: { likes: req.user._id } }, {
       new: true,
     });
+    if (!cards) {
+      return res.status(404).send({ message: 'Передан несуществующий _id карточки' });
+    }
     res.status(200).send(cards);
   } catch (err) {
     if (err.name === 'CastError') {
-      return res.status(404).send({ message: 'Передан несуществующий _id карточки' });
-    }
-    if (err.name === 'NotValididId') {
       return res.status(400).send({ message: 'Переданы некорректные данные' });
     }
     res.status(500).send({ message: 'Oшибка по-умолчанию', ...err });
