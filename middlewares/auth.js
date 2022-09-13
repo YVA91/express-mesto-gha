2fc849b1906f343e9927a789906d8931796cc918
Vladimir Iakhnev <Yahnev91@yandex.ru>
2022-09-13 21:54:02 +0300

    ПР14 автотесты

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,7 +5,7 @@ module.exports = (req, res, next) => {
   const token = req.cookies.jwt;
   let payload;
   try {
-    payload = jwt.verify(token, process.env.JWT_SECRET);
+    payload = jwt.verify(token, 'SECRET');
   } catch (err) {
     next(new UnauthorizedError('Сначала нужно авторизироваться'));
   }
